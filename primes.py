a4d43c5b073c6f673b5841c637aaa8f0388d08e7
Sydney <k2002418@kcl.ac.uk>
2022-10-14 00:41:03 +0100

    update

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    
+    if number_of_primes<1:
+        raise ValueError("Numbers cannot be zero or negative.")
+    
+    list = [] 
+    i=2
+    
+    while len(list) != number_of_primes:
+        if isPrimes(i):
+            list.append(i)
+            i+=1
+        else:
+            i+=1
+                 
     return list
+    
+def isPrimes(n):
+    for k in range(2,n):
+        if n%k == 0:
+            return False
+    else:
+            return True
